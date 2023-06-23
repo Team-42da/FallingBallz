4934d3bc74eeb813c710a670dfc1173fb94d1b39
chaewon-moana <dreamytaurus508@gmail.com>
2023-06-23 11:36:03 +0900

    physicbody로 padding 값을 줬으나 블록들끼리의 충돌은 해결하지 못 함,,,

--- FallingBallz/GameScene.swift
+++ FallingBallz/GameScene.swift
@@ -1,88 +1,90 @@
-//
-//  GameScene.swift
-//  FallingBallz
-//
-//  Created by Greed on 2023/06/18.
-//
 
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var obstacleNumber : Int = 0
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+    // 누르면 볼이 향할 방향 나타내기
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        
+        let randomObsacleNumber = Int.random(in: 1...3)
+        if randomObsacleNumber == 1 {
+            makeObstacle()
+        } else if randomObsacleNumber == 2{
+            makeObstacle()
+            makeObstacle()
+        } else {
+            makeObstacle()
+            makeObstacle()
+            makeObstacle()
+        }
+
+        
+        self.enumerateChildNodes(withName: "//obstacle*") { (node, stop) in
+            node.run(SKAction.moveBy(x: 0, y: self.frame.size.height * 0.15, duration: 0.1))
+            if node.position.y > -800 + self.frame.size.height * 0.15 * 5 {
+                node.removeFromParent()
+            }
+        }
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func randomPosX() -> Int {
+        let randomX = Int.random(in: -210...210)
+        return randomX
+    }
+    
+    func randomHuddle() -> String {
+        let randomShape = Int.random(in: 0...2)
+        let shapeName: String
+        
+        switch randomShape {
+        case 0:
+            shapeName = "Triangle"
+        case 1:
+            shapeName = "Circle"
+        case 2:
+            shapeName = "Rectangle"
+        default:
+            shapeName = "Circle"
+        }
+        
+        return shapeName
+    }
+    
+    func makeObstacle() {
+        let randomX = randomPosX()
+        let randomShape = randomHuddle()
+        let obstacle = SKSpriteNode(imageNamed: "huddle\(randomShape)")
+        let obstaclePadding = SKPhysicsBody(rectangleOf: CGSize(width: obstacle.size.width+30, height: obstacle.size.height+20))
+        obstaclePadding.affectedByGravity = false
+        //body.usesPreciseCollisionDetection = true  //충돌감지할 때 사용
+        
+        obstacle.size = CGSize(width: frame.size.width * 0.2 , height: frame.size
+            .height * 0.1)
+        obstacle.position = CGPoint(x: randomX, y: -800)
+        obstacle.name = "obstacle\(obstacleNumber)"
+       
+        obstacleNumber += 1
+        obstacle.physicsBody = obstaclePadding
+        
+        self.addChild(obstacle)
+        
+        let heightScale = frame.size.height * 0.1
+        
+        obstacle.run(SKAction.moveBy(x: 0, y: heightScale, duration: 0.1))
     }
     
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
+
+
