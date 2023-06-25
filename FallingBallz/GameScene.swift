99b423a02f82b78cf2cb01c9ae0484d6bea9ab99
Greed <suby0475@gmail.com>
2023-06-25 16:41:43 +0900

    노드 클릭시 해당 노드 카운트 - 1 / 0되면 사라지게 변경

--- FallingBallz/GameScene.swift
+++ FallingBallz/GameScene.swift
@@ -31,9 +31,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(scoreLabel)
     }
     
-    // 누르면 볼이 향할 방향 나타내기
+    // 누르면 볼이 향할 방향 나타내기 해야함
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //클릭하면 노드카운트 줄이기
+        guard let touch = touches.first else { return }
+        let touchLocation = touch.location(in: self)
         
+        // 터치된 노드를 찾습니다.
+        let touchedNode = self.atPoint(touchLocation)
+        
+        // 터치된 노드가 `obstacle`인지 확인하고, nodeCount를 줄입니다.
+        if let obstacleNode = touchedNode as? SKSpriteNode, obstacleNode.name?.hasPrefix("obstacle") == true {
+            decreaseNodeCount(obstacleNode)
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -49,16 +59,34 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             makeObstacle()
             makeObstacle()
         }
-
+        
         
         self.enumerateChildNodes(withName: "//obstacle*") { (node, stop) in
             node.run(SKAction.moveBy(x: 0, y: self.frame.size.height * 0.11, duration: 0.1))
             if node.position.y > -800 + self.frame.size.height * 0.11 * 8 {
                 node.removeFromParent()
-                self.score += 1
+            }
+            
+        }
+    }
+    
+    func decreaseNodeCount(_ obstacleNode: SKSpriteNode) {
+        // obstacleNode에서 countLabel을 찾습니다.
+        if let countNode = obstacleNode.childNode(withName: "countLabel") as? SKLabelNode {
+            // 현재 nodeCount를 가져옵니다.
+            if let countText = countNode.text, let count = Int(countText) {
+                // nodeCount를 1씩 감소시키고, 0보다 작으면 노드를 제거합니다.
+                let newCount = count - 1
+                if newCount > 0 {
+                    countNode.text = "\(newCount)"
+                } else {
+                    obstacleNode.removeFromParent()
+                    self.score += 1
+                }
             }
         }
     }
+
     
     func randomPosX() -> Int {
         let randomX = Int.random(in: -300...300)
@@ -82,17 +110,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         return shapeName
     }
     
+    // MARK: - 장애물생성
     func makeObstacle() {
+        //랜덤 모양, 위치 생성
         let randomX = randomPosX()
         let randomShape = randomHuddle()
         let obstacle = SKSpriteNode(imageNamed: "huddle\(randomShape)")
         obstacle.size = CGSize(width: frame.size.width * 0.15 , height: frame.size
             .height * 0.08)
+        
         obstacle.position = CGPoint(x: randomX, y: -600)
         obstacle.name = "obstacle\(obstacleNumber)"
         obstacleNumber += 1
         
-        let obstaclePadding = SKPhysicsBody(rectangleOf: CGSize(width: obstacle.size.width + 30 , height: obstacle.size.height + 10 ))
+        //physics
+        let obstaclePadding = SKPhysicsBody(rectangleOf: CGSize(width: obstacle.size.width + 50 , height: obstacle.size.height + 10 ))
         obstacle.physicsBody = obstaclePadding
         obstacle.physicsBody!.friction = 0.0
         obstacle.physicsBody!.isDynamic = true
@@ -100,6 +132,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         obstacle.physicsBody?.affectedByGravity = false
         obstacle.zPosition = 1
         
+        //count 추가
         let labelNode = addCount()
         obstacle.addChild(labelNode)
         self.addChild(obstacle)
@@ -113,18 +146,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let nodeCount: Int
         
         switch score {
-        case 0...10:
-            nodeCount = Int.random(in: 1...3)
+        case 0...5:
+            nodeCount = Int.random(in: 1...2)
+        case 6...10:
+            nodeCount = Int.random(in: 2...4)
         case 11...20:
-            nodeCount = Int.random(in: 4...10)
+            nodeCount = Int.random(in: 4...6)
         case 21...40:
-            nodeCount = Int.random(in: 11...20)
-        case 41...60:
-            nodeCount = Int.random(in: 21...30)
+            nodeCount = Int.random(in: 7...10)
         default:
-            nodeCount = Int.random(in: 1...100)
+            nodeCount = Int.random(in: score/5 - 2 ... score/5 + 3)
         }
         let labelNode = SKLabelNode(text: "\(nodeCount)")
+        labelNode.name = "countLabel"
         labelNode.fontColor = UIColor.black
         labelNode.fontSize = 40
         labelNode.fontName = "SFUI-Regular"
